72b847cbaad9736e78d8d44cf4c62c8461367f65
easycastle <sean34418290@gmail.com>
2021-06-07 03:14:04 +0900

    fix : change interactive Body

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ window = Tk()
 window.title('n-body simulator')
 
 
-canvas = Canvas(window, width=700, height=700, bg='black')
-canvas.grid(row=0, column=0)
+space = Canvas(window, width=700, height=700, bg='black')
+space.grid(row=0, column=0)
 
 G = 0.01                                                        # gravitational constant
 Bodies = []                                                     # list of Bodies
@@ -26,27 +26,27 @@ class Body:
         if self == interaction:
             self.dV = np.array([0, 0], dtype=float)
         else:
-            self.distance = sqrt((self.P[0] - interaction.P[0]) ** 2 + (self.P[1] - interaction.P[1]) ** 2)
+            distance = sqrt((self.P[0] - interaction.P[0]) ** 2 + (self.P[1] - interaction.P[1]) ** 2)
             self.dV = np.array([0, 0], dtype=float)
 
-            if self.distance == 0:
+            if distance == 0:
                 self.dV = 0
             else:
-                self.F = (-G * self.m * interaction.m / self.distance ** 2) * (self.P - interaction.P) / self.distance
+                self.F = (-G * self.m * interaction.m / distance ** 2) * (self.P - interaction.P) / distance
                 self.dV = self.F / self.m
 
-                if self.distance < self.r + interaction.r + 5:
-                    self.dV *= -0.5
+                if distance < self.r + interaction.r + 5:
+                    self.dV *= -1
 
         self.V += self.dV
 
-        if self.P[0] < 10 + self.r:
+        if self.P[0] < 2 + self.r:
             self.V[0] *= -0.9
             self.P[0] += 1
         if self.P[0] > 698 - self.r:
             self.V[0] *= -0.9
             self.P[0] -= 1
-        if self.P[1] < 10 + self.r:
+        if self.P[1] < 2 + self.r:
             self.V[1] *= -0.9
             self.P[1] += 1
         if self.P[1] > 698 - self.r:
@@ -57,19 +57,19 @@ class Body:
 B = 2
 
 for i in range(B):
-    Bodies.append(Body(randrange(100, 1000), randrange(50, 650), randrange(50, 650), [0.005, 0.001]))
+    Bodies.append(Body(randrange(100, 1000), randrange(50, 650), randrange(50, 650), [(randrange(-1, 1) / 20), (randrange(-1, 1) / 20)]))
 
 T = 0
 Tx = True
 while Tx == True:
     T += 1
 
-    canvas.delete('all')
+    space.delete('all')
 
     for Body1 in Bodies:
         for Body2 in Bodies:
             Body1.Fg(Body2)
-        Body1.P += Body2.V
+            Body1.P += Body1.V
 
         if Body1.P[0] < 5:
             Body1.V[0] *= -0.5
@@ -81,9 +81,9 @@ while Tx == True:
         if Body1.P[1] > 695:
             Body1.V[1] *= -0.5
 
-        canvas.create_oval(Body1.P[0] - Body1.r, Body1.P[1] - Body1.r, Body1.P[0] + Body1.r, Body1.P[1] + Body1.r, fill='skyblue')
+        space.create_oval(Body1.P[0] - Body1.r, Body1.P[1] - Body1.r, Body1.P[0] + Body1.r, Body1.P[1] + Body1.r, fill='skyblue')
 
-    canvas.update()
+    space.update()
 
     if T == 100000:
         Tx = False
